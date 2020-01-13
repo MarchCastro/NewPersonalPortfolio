f056e5ee1c87285316331c529ed13c158ad2875a
MarcelaCF <marcela.cfloress@gmail.com>
2020-01-13 17:44:00 -0600

    created vue project

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <span>hola</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+.card
+  width: 400px
+
+  .tarjetita
+    color: blue
+</style>
