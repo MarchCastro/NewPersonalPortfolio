43f87eefc873c5a501cc4a1d19c61ece9aa33c54
MarcelaCF <marcela.cfloress@gmail.com>
2020-01-16 01:35:09 -0600

    cards with icons finished

--- src/components/ProfessionalBackgroundCard.vue
+++ src/components/ProfessionalBackgroundCard.vue
@@ -6,8 +6,28 @@
 
       <div class="subtitle-page"> Here's a little bit about my professional skills </div>
       
+      
       <div class="card-div">
-        <div class="skill-card"></div>
+        <!--Languages card -->
+        <div class="skill-card">
+          <div class="top-card-rectangle"></div>
+          <img class="card-icon" src="../assets/img/languages.png">
+        </div>
+        <!--Technologies card -->
+        <div class="skill-card">
+          <div class="top-card-rectangle"></div>
+          <img class="card-icon" src="../assets/img/technologies.png">
+        </div>
+        <!--Skills card -->
+        <div class="skill-card">
+          <div class="top-card-rectangle"></div>
+          <img class="card-icon" src="../assets/img/skills1.png">
+        </div>
+        <!--Tools card -->
+        <div class="skill-card">
+          <div class="top-card-rectangle"></div>
+          <img class="card-icon" src="../assets/img/code1.png">
+        </div>
       </div>
     </div>
   </div>
@@ -36,16 +56,31 @@ export default {
     margin-top: 7px
   
   .card-div
-    margin-left: 59px
+    margin-left: 50px
     margin-top: 43px
     
   .skill-card
     border-radius: 8px
     box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5)
     height: 330px
-    margin-right: 56px
+    margin: 25px
     width: 261px
-     
+    display: inline-block
+  
+  .top-card-rectangle
+    background-color: #875970
+    height: 10px
+    width: 261px 
+    border-top-right-radius: 8px
+    border-top-left-radius: 8px
+  
+  .card-icon  
+    height: 72px
+    margin-left: 95px
+    margin-top: 38px
+    width: 72px
+
+
   @media screen and (min-width: 1440px)
     
 
