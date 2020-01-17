6f253336fb97e10601205c538272c585fcd78715
MarcelaCF <marcela.cfloress@gmail.com>
2020-01-17 02:03:56 -0600

    decorative ovals

--- src/components/EducationCard.vue
+++ src/components/EducationCard.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="educationCard" class="section">
     <div class="card">
-      <div class="title-page"> Education </div>
+      <!-- Decoration ovals -->
+      <div class="oval one purple-one"></div>
+      <div class="oval two purple-four"></div>
+      <div class="oval three purple-three"></div>
+      <div class="oval four purple-two"></div>
+      <div class="oval five purple-one"></div>
+      <div class="oval six purple-two"></div>
+      <div class="oval seven purple-four"></div>
 
+      <!-- Title page -->
+      <div class="title-page"> Education </div>
       <div class="subtitle-page"> Here, you can find the places where I've learned. </div>
       
       <!-- Cards title -->
@@ -73,7 +82,7 @@ export default {
     height: 28px
     margin-left: 65px
     margin-right: 65px
-    margin-top: 64px
+    margin-top: 30px
 
   .card-title
     color: #364252
@@ -147,5 +156,64 @@ export default {
   .education-card:hover .overlay 
     height: 100%
   
+  .oval
+    border-radius: 50%
+    position: absolute
+
+    &.purple-one
+      background: #843b62
+
+    &.purple-two
+      background:  #74546a
+
+    &.purple-three
+      background: #6d435a
+
+    &.purple-four
+      background: #352d39
+
+  @media screen and (min-width: 1440px)
+    .oval
+      &.one
+        height: 18px
+        left: 1201px
+        top: 70px
+        width: 18px
+
+      &.two
+        height: 60px
+        left: 1225px
+        top: 30px
+        width: 60px
+
+      &.three
+        height: 9px
+        left: 1270px
+        top: 100px
+        width: 9px
+
+      &.four
+        height: 25px
+        left: 1290px
+        top: 90px
+        width: 25px
+
+      &.five
+        height: 14px
+        left: 22px
+        top: 627px
+        width: 14px
+
+      &.six
+        height: 45px
+        left: 13px
+        top: 678px
+        width: 45px
+
+      &.seven
+        height: 22px
+        left: 62px
+        top: 666px
+        width: 22px
   
 </style>
